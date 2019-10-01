19b8b319134065e46dc5fd92635b8e784ef17084
Dakota McCombs <42280934+d82mc@users.noreply.github.com>
2019-10-01 15:36:50 -0600

    Still working!
    Bug fixes, added resume link

---
+++ precache-manifest.093f3c11cad70ad6e989ae65e39124f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c6cc44535e201e9923bc84aa103c251",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b3afdde2a9f6684d9959",
+    "url": "/portfolio/static/css/main.be69274a.chunk.css"
+  },
+  {
+    "revision": "575982e2cf24c90446ee",
+    "url": "/portfolio/static/js/2.cdd3509a.chunk.js"
+  },
+  {
+    "revision": "b3afdde2a9f6684d9959",
+    "url": "/portfolio/static/js/main.4bf03cb3.chunk.js"
+  },
+  {
+    "revision": "c98fad0fa5c777d28f1b",
+    "url": "/portfolio/static/js/runtime-main.36963274.js"
+  },
+  {
+    "revision": "be6354fda0c341df8cbd60d5d4a394dc",
+    "url": "/portfolio/static/media/background.be6354fd.png"
+  }
+]);
