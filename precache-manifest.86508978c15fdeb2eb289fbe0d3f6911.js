98914c91f9b5f4db14f3de6892eb82e43980dcad
Dakota McCombs <42280934+d82mc@users.noreply.github.com>
2019-10-03 13:29:50 -0600

    Removed slider for page stability

---
+++ precache-manifest.86508978c15fdeb2eb289fbe0d3f6911.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86b7649c543085f905ee27fbb1a84ecc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42346298c625caa4a95c",
+    "url": "/static/css/main.0902254f.chunk.css"
+  },
+  {
+    "revision": "24faf233b39f874bdb8a",
+    "url": "/static/js/2.dad9b437.chunk.js"
+  },
+  {
+    "revision": "42346298c625caa4a95c",
+    "url": "/static/js/main.9611fa44.chunk.js"
+  },
+  {
+    "revision": "a6e3dc98bf14d00cdb8a",
+    "url": "/static/js/runtime-main.c5683173.js"
+  },
+  {
+    "revision": "a2395d6b488c324f3a55533eeb361b25",
+    "url": "/static/media/background.a2395d6b.jpeg"
+  }
+]);
